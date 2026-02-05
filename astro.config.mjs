2a1d91d70bfdf521788b303d34da629213d19e66
Lohtharr <manuel@oelmaier.eu>
2026-02-05 20:58:26 +0100

    100 everywhere lighthouse ?

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,8 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://manueloelmaier.de',
   outDir: './dist',
+  build: {
+    // This will take that 2.2 KiB CSS and put it directly into the HTML
+    inlineStylesheets: 'always',
+  },
 });
