9b747b7d7fa02800de1cbc2314a7fec51258bca5
Lohtharr <manuel@oelmaier.eu>
2026-02-08 12:49:02 +0100

    fixes

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,10 +2,7 @@
 import { defineCollection } from 'astro:content';
 
 // 2. Import loader(s)
-import { glob, file } from 'astro/loaders';
-
-// 3. Import Zod
-import { z } from 'astro/zod';
+import { glob } from 'astro/loaders';
 
 // 4. Define your collection(s)
 const blog = defineCollection({
