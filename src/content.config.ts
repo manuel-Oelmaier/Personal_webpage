454c299ea2ce164355442d279d69e5ebccff7e0a
Lohtharr <manuel@oelmaier.eu>
2026-02-08 12:39:33 +0100

    content but with errors

---
+++ src/content.config.ts
@@ -0,0 +1,18 @@
+// 1. Import utilities from `astro:content`
+import { defineCollection } from 'astro:content';
+
+// 2. Import loader(s)
+import { glob, file } from 'astro/loaders';
+
+// 3. Import Zod
+import { z } from 'astro/zod';
+
+// 4. Define your collection(s)
+const blog = defineCollection({
+    loader: glob({ pattern: "**/*.html", base: "./src/content" }),
+    schema: /* ... */
+});
+
+
+// 5. Export a single `collections` object to register your collection(s)
+export const collections = { blog };
